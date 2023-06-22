94ce615881664863c610fe1d24f4a369bb14674c
Fischer14 <rfischerpaez@gmail.com>
2023-06-22 21:18:44 +0200

    cfu-vue

---
+++ cfu-ejercicios/cfu-vue/cfu-directivas/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+const active = ref(true);
+</script>
+
+<template>
+  <div>
+    <button v-bind:disabled="active">Subscribe</button>
+  </div>
+</template>
+
+<style scoped></style>
